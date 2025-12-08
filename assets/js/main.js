64ab70f192ab48dfb9085356c48c1cbbf01ee9c7
Davis Hoffstatter <dhoffstatter@uiowa.edu>
2025-12-08 17:26:02 -0600

    Hamburger Style Nav BAr

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,45 +6,47 @@
 
 (function($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	breakpoints({
+		xlarge:   [ '1281px',  '1680px' ],
+		large:    [ '981px',   '1280px' ],
+		medium:   [ '737px',   '980px'  ],
+		small:    [ '481px',   '736px'  ],
+		xsmall:   [ '361px',   '480px'  ],
+		xxsmall:  [ null,      '360px'  ]
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Mobile?
-		if (browser.mobile)
+	if (browser.mobile)
+		$body.addClass('is-mobile');
+	else {
+		breakpoints.on('>medium', function() {
+			$body.removeClass('is-mobile');
+		});
+
+		breakpoints.on('<=medium', function() {
 			$body.addClass('is-mobile');
-		else {
-
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
-
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
-
-		}
+		});
+	}
 
 	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500
-			});
+	$('.scrolly').scrolly({
+		speed: 1500
+	});
+
+	// ✅ Nav toggle for mobile
+	$('.nav-toggle').on('click', function() {
+		$('.nav-links').toggleClass('show');
+	});
 
 })(jQuery);
